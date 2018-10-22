c964a0829868ffa0dbbf421a6ebc39e555480961
Bart Malfait <malfaitbart@gmail.com>
2018-10-22 16:16:01 +0200

    update

---
+++ FVD.Tests/ProfessorController_Test.cs
@@ -0,0 +1,27 @@
+﻿using FunnyVersityDeluxe.Controllers.Professors;
+using FVD.Services.Interfaces;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace FVD.Tests
+{
+    public class ProfessorController_Test
+    {
+        //[Fact]
+        //public void GivenAProfessorController_WhenCallingGetAllProffessors_GetAListOfProfessors()
+        //{
+        //    //Given
+        //    var professorServiceSUT = Substitute.For<IProfessorService>();
+
+        //    ProfessorsController professorController = new ProfessorsController(professorServiceSUT, professorMapper);
+
+
+        //    //When
+
+        //    //Then
+        }
+    }
+}
